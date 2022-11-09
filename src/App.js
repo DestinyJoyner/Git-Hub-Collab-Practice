4153737021b84a7c330f9c53e2866f1fbbf07656
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-09 10:43:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Pursuit App Template</h1>
+      <h1>Git Hub Collab Practice</h1>
+      <p>Add Edits Here</p>
     </div>
   );
 }
